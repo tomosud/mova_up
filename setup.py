a7dd426b89ea1121eb44bdfc966a89df9ee84221
tomosud <tomohisaoda@gmail.com>
2024-04-14 09:55:18 +0900

    move
    move

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='popro_control',
+    version='0.12',
+    packages=find_packages(),
+    install_requires=[
+        'requests',  # 依存関係としてrequestsを追加
+        'dearpygui==1.11.1',  # dearpyguiのバージョンを1.11.1に指定
+        'tzdata',  # tzdataを追加 タイムゾーンの設定に使用
+        'aiohttp',  # aiohttpを追加
+        'aiofiles',  # aiofilesを追加
+    ]
+)
